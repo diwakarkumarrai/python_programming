2703a37011698ed92b219027d9fa9f0674f04ea9
Diwakar kumar <bhaidiwakar30@gmail.com>
2026-07-03 23:00:40 +0530

    code

--- lec_2.py
+++ lec_2.py
@@ -1,6 +1,6 @@
 # Take input from the user
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+num1 = float(input("Enter first number:"))
+num2 = float(input("Enter second number:"))
 
 # Calculate sum
 sum = num1 + num2
